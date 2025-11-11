ac9cf53f8f7f6502eec3de8c384968f0bbfc05ca
Nischhelm <nischhelm@gmail.com>
2025-11-11 23:22:05 +0100

    v1.4.1
    fixed serverside crash

--- src/main/java/fermiumbooter/FermiumBooter.java
+++ src/main/java/fermiumbooter/FermiumBooter.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class FermiumBooter {
 	
     public static final String MODID = "fermiumbooter";
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
     public static final String NAME = "FermiumBooter";
 	
 	@SidedProxy(clientSide = "fermiumbooter.proxy.ClientProxy", serverSide = "fermiumbooter.proxy.CommonProxy")
